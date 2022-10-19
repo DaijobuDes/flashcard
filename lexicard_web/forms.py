e7a3a3007c1e4f9efc19b977c290bf1d6cd1681d
Kate Aubrey Cellan <cellankateaubrey@gmail.com>
2022-10-19 15:28:29 +0800

    Add forms.py for form handling in HTML and in views.py

---
+++ lexicard_web/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from .models import *
+
+class UserForm(forms.Form):
+   """
+   A class used for login
+   """
+
+   email = forms.EmailField(widget=forms.EmailInput())
+   password = forms.CharField(widget=forms.PasswordInput())
+
+   class Meta:
+      """
+      Class metadata for UserForm
+      """
+      model = User
+      fields = ['email', 'password']
+
+
+class RegistrationForm(forms.Form):
+    """
+    A class used for user registration
+    """
+
+    username = forms.CharField(widget=forms.CharField())
+    email = forms.EmailField(widget=forms.EmailInput())
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        """
+        Class metadata for RegistrationForm
+        """
+        model = User
+        fields = ['username', 'email', 'password']

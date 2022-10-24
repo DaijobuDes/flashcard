c8211ca5b324a4283a817c30cf87e4c92208c553
densentsu124 <84673058+densentsu124@users.noreply.github.com>
2022-10-24 21:28:11 +0800

    logout buttons on the headers

--- flashcard_frontend/urls.py
+++ flashcard_frontend/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', views.Login.as_view()),
     path('register/', views.Register.as_view()),
+    path('logout/', views.Logout.as_view()),
 ]
